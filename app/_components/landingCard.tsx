ef0260808e4d6af0c6803c5fefa7adeb33348158
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2025-01-08 18:25:31 +0530

    minor ux fix

--- app/_components/landingCard.tsx
+++ app/_components/landingCard.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useUser } from './UserContext';
 
 function LandingCard() {
  
-  
-    
+  const {isLoggedIn,username} = useUser();
+  const [isClient,setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return <div></div>;
     
   return (
     <div 
@@ -16,8 +23,11 @@ function LandingCard() {
         {/* <p className="capitalize text-base sm:text-xl text-white">👋 Welcome! Please start by telling us your name:</p> */}
         {/* <input type="text" id='name' name='name' max={50} minLength={2} onChange={handleChange}/>
         <input type="text" id='password' name='password' maxLength={8} minLength={8} onChange={handleChange}/> */}
-        
-        <Link href={"/login"} className="bg-yellow-400 py-2 px-3 capitalize rounded-xl mt-5 sm:mt-7 w-full sm:w-1/4 text-xl">Login-Now</Link>
+        {
+          (!isLoggedIn||!username)?<Link href={"/login"} className="bg-yellow-400 py-2 px-3 capitalize rounded-xl mt-5 sm:mt-7 w-full sm:w-1/4 text-xl">Login-Now</Link>:
+            <Link href={"/login"} className="bg-yellow-400 py-2 px-3 capitalize rounded-xl mt-5 sm:mt-7 w-full sm:w-1/4 text-xl">Book-Table Now</Link>
+          
+        }
         
       {/* </form> */}
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import UserLoginForm from "./_components/landingCard";
 export default function Home() {
   return (
     
-   <div className="h-full relative bg-gray-700">
+   <div className="h-full w-full relative bg-gray-700">
       <Image className="object-cover object-center opacity-50"  src="/landingImage.jpg" fill alt="Restaurant image" quality={50}/>
       <div className="relative z-10 h-full mx-auto">
         <UserLoginForm></UserLoginForm>
